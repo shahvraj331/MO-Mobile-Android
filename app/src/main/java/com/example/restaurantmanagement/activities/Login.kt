4a7230b5842e37d9ce7a8de7bfb462fab365093d
mansiprajapati169 <mansiprajapati169@gmail.com>
2022-04-02 11:30:59 +0530

    login authentication

---
+++ app/src/main/java/com/example/restaurantmanagement/activities/Login.kt
@@ -0,0 +1,80 @@
+package com.example.restaurantmanagement.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.widget.doOnTextChanged
+import com.example.restaurantmanagement.MainActivity
+import com.example.restaurantmanagement.R
+import com.example.restaurantmanagement.databinding.ActivityLoginBinding
+import com.google.android.material.textfield.TextInputLayout
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val actionBar = supportActionBar
+        actionBar?.title = getString(R.string.login_activity)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        loginAuthentication()
+    }
+
+    private fun loginAuthentication() {
+
+        binding.etEmail.doOnTextChanged { text, _, _, _ ->
+            checkTextExistence(text.toString(), binding.tiEmail)
+        }
+
+        binding.etPassword.doOnTextChanged { text, _, _, _ ->
+            checkTextExistence(text.toString(), binding.tiPassword)
+        }
+
+        binding.etEmail.doOnTextChanged { text, _, _, _ ->
+            if (text != "") {
+                binding.tiEmail.error = null
+            }
+        }
+
+        binding.etPassword.doOnTextChanged { text, _, _, _ ->
+            if (text != "") {
+                binding.tiPassword.error = null
+            }
+        }
+
+        binding.btnLogin.setOnClickListener {
+            if (binding.etEmail.text.toString() == "" || binding.etPassword.text.toString() == "") {
+                binding.tiEmail.error = getString(R.string.txt_required)
+                binding.tiPassword.error = getString(R.string.txt_required)
+            }
+            else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.text.toString()).matches()) {
+                binding.tiEmail.error = getString(R.string.txt_invalid_email)
+            }
+            else {
+                goToHome()
+            }
+        }
+    }
+
+    private fun checkTextExistence(text: String, view: TextInputLayout) {
+        if (text != "") {
+            view.error = null
+        }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
