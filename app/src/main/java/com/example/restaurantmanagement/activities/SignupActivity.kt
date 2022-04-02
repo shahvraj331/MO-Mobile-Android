8578e23fbf6d0685fd331d0ab8d7ef54ead1ca75
shahvraj331 <shahvraj2016@gmail.com>
2022-04-02 20:48:23 +0530

    Finalized authentication done

--- app/src/main/java/com/example/restaurantmanagement/activities/SignupActivity.kt
+++ app/src/main/java/com/example/restaurantmanagement/activities/SignupActivity.kt
@@ -8,35 +8,34 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.core.widget.doOnTextChanged
 import com.example.restaurantmanagement.R
-import com.example.restaurantmanagement.databinding.ActivitySignupBinding
+import com.example.restaurantmanagement.databinding.ActivitySignUpBinding
 import com.google.android.material.textfield.TextInputLayout
 
 class SignupActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignupBinding
+    private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val actionBar = supportActionBar
         actionBar?.title = getString(R.string.sign_up_activity)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.baseLayout.setOnClickListener {
+        binding.baselayout.setOnClickListener {
             if (currentFocus != null) {
                 val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
             }
         }
 
-        onTextChanged(binding.etUsername, binding.tlUsername)
-        onTextChanged(binding.etEmail, binding.tlEmail)
-        onTextChanged(binding.etPassword,binding.tlPassword)
-        onTextChanged(binding.etConfirmPassword, binding.tlConfirmPassword)
+        onTextChanged(binding.etUsername, binding.tiUsername)
+        onTextChanged(binding.etEmail, binding.tiEmail)
+        onTextChanged(binding.etPassword,binding.tiPassword)
 
-        binding.tvGoToSignin.setOnClickListener {
+        binding.tvSignin.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
@@ -47,15 +46,12 @@ class SignupActivity : AppCompatActivity() {
     }
 
     private fun validation() {
-        if (binding.etUsername.text.toString() == "" || binding.etEmail.text.toString() == "" || binding.etPassword.text.toString() == "" || binding.etConfirmPassword.text.toString() == "") {
-            binding.tlEmail.error = getString(R.string.txt_required)
-            binding.tlUsername.error = getString(R.string.txt_required)
-            binding.tlPassword.error = getString(R.string.txt_required)
-            binding.tlConfirmPassword.error = getString(R.string.txt_required)
+        if (binding.etUsername.text.toString() == "" || binding.etEmail.text.toString() == "" || binding.etPassword.text.toString() == "") {
+            binding.tiEmail.error = getString(R.string.txt_required)
+            binding.tiUsername.error = getString(R.string.txt_required)
+            binding.tiPassword.error = getString(R.string.txt_required)
         } else if ( !android.util.Patterns.EMAIL_ADDRESS.matcher(binding.etEmail.text.toString()).matches() ){
-            binding.tlEmail.error = getString(R.string.txt_invalid_email)
-        } else if ( binding.etPassword.text.toString() != binding.etConfirmPassword.text.toString()) {
-            binding.tlConfirmPassword.error = getString(R.string.txt_password_mismatch)
+            binding.tiEmail.error = getString(R.string.txt_invalid_email)
         } else {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
