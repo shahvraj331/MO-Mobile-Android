0f16d1e13284cfd1354571998f69377aa41f2ae0
AparnaTati <aparna.t@simformsolutions.com>
2022-04-03 12:31:07 +0530

    ADD: Favourite List

--- app/src/main/java/com/example/restaurantmanagement/activities/DineInFragment.kt
+++ app/src/main/java/com/example/restaurantmanagement/activities/DineInFragment.kt
@@ -5,15 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.restaurantmanagement.R
+import com.example.restaurantmanagement.databinding.FragmentDineInBinding
+import com.example.restaurantmanagement.menulist.MyMenuAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DineInFragment : Fragment() {
+
+    private lateinit var binding: FragmentDineInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_dine_in, container, false)
+        binding = FragmentDineInBinding.inflate(layoutInflater)
+        setTabLayout()
+        return binding.root
     }
+
+    private fun setTabLayout() {
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager
+
+        val title = arrayOf("Menu", "Table")
+
+        viewPager.adapter = MyMenuAdapter(parentFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, pos ->
+            tab.text = title[pos]
+        }.attach()
+    }
+
 }
--- app/src/main/java/com/example/restaurantmanagement/menulist/MenuFragment.kt
+++ app/src/main/java/com/example/restaurantmanagement/menulist/MenuFragment.kt
@@ -1,13 +1,11 @@
 package com.example.restaurantmanagement.menulist
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.restaurantmanagement.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -20,12 +18,16 @@ class MenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMenuBinding.inflate(layoutInflater)
+        setUpRecyclerView()
+        return binding.root
+    }
+
+    private fun setUpRecyclerView() {
         val menuRecyclerView = binding.menuRecyclerView
         menuRecyclerView.setHasFixedSize(true)
         val layoutManager = GridLayoutManager(context, 2)
         menuRecyclerView.layoutManager = layoutManager
-        menuRecyclerView.adapter = MyMenuFragmentAdapter(Data.getMenuData())
-        Log.d("Key", menuRecyclerView.toString())
-        return binding.root
+        menuRecyclerView.adapter = context?.let { MyMenuFragmentAdapter(Data.getMenuData(), it) }
     }
+
 }
