655f9211c2fd9604febbb5cfe8ca90fe7702c4ce
Dhyana Dave <dhyana.d@simformsolutions.com>
2022-04-03 11:46:19 +0530

    Rating first commit

---
+++ app/src/main/java/com/example/restaurantmanagement/activities/RatingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.restaurantmanagement.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurantmanagement.R
+
+class RatingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ratings)
+    }
+}
